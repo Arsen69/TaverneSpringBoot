512b1f93a0815032cf81fdb7d64d1a9dfadb38c8
Erwan <erwan.beving@gmail.com>
2022-01-16 16:25:12 +0100

    hotfix

--- Taverne/src/app/services/comptes.service.ts
+++ Taverne/src/app/services/comptes.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComptesService {
+  private static URL: string = 'http://localhost:8080/taverne/comptes';
+  constructor(private http: HttpClient, private auth: AuthenticationService) {}
 
-  constructor() { }
+
+
 }
