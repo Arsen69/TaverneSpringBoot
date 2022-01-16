eb22e4b1c2de198c7e24f3803d0cde963931cc68
Erwan <erwan.beving@gmail.com>
2022-01-16 18:04:03 +0100

    added all fonctionnalitees in model

--- Taverne/src/app/services/comptes.service.ts
+++ Taverne/src/app/services/comptes.service.ts
@@ -1,13 +1,24 @@
+import { Compte } from './../model/comptes/compte';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ComptesService {
   private static URL: string = 'http://localhost:8080/taverne/comptes';
   constructor(private http: HttpClient, private auth: AuthenticationService) {}
 
+  public getAll(): Observable<Compte[]> {
+    return this.http.get<Compte[]>(ComptesService.URL, {
+      headers: this.auth.headers,
+    });
+  }
 
-
+  public getById(id: number): Observable<Compte> {
+    return this.http.get<Compte>(`${ComptesService.URL}/${id}`, {
+      headers: this.auth.headers,
+    });
+  }
 }
