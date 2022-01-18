7b17e848309f2f4f25b273ce206f50cd688f73ac
Antoine SPANIER <antoine.spanier@gmail.com>
2022-01-18 21:39:17 +0100

    travail sur formulaire inscription

--- Taverne/src/app/CustomValidators/login-unique.ts
+++ Taverne/src/app/CustomValidators/login-unique.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@angular/core';
 import {
   AbstractControl,
   AsyncValidatorFn,
@@ -6,12 +7,15 @@ import {
 import { debounceTime, map, Observable } from 'rxjs';
 import { CheckDataService } from '../services/Users/check-data.service';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class LoginUnique {
   constructor(private checkDataService: CheckDataService) {}
 
-  checkPasDansListe(controlvalue: string): AsyncValidatorFn {
+  public checkPasDansListe(): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
-      return this.checkDataService.checkDataInList(controlvalue).pipe(
+      return this.checkDataService.checkDataInList(control.value)!.pipe(
         debounceTime(1000),
         map((res: boolean) => {
           return res ? { checkDataInList: true } : null;
